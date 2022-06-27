3c6bcf6425e79d986877c4ff6ac84896f25789eb
austinFlipside <austin@flipsidecrypto.com>
2022-06-27 13:32:14 -0400

    commit

--- macros/db_comment.sql
+++ macros/db_comment.sql
@@ -4,10 +4,15 @@ SELECT
     TO_DATE(MIN(block_timestamp))
 FROM
     silver.blocks {% endset %}
+    {{ log(query) }}
+
     {% set results = run_query(query) %}
+    {% do results.print_table() %}
     {% set results_list = results.columns [0].values() [0].strftime('%Y-%m-%d') %}
+    {{ log(results_list) }}
+
     {% set sql %}
     COMMENT
-    ON database bsc_dev IS 'Lite Mode dataset with recent data only. Min block_timestamp: {{ results_list }} 🌱 ' {% endset %}
+    ON database bsc_dev IS 'Lite Mode dataset with recent data only. Min block_timestamp:{{results_list}} 🌱 ' {% endset %}
     {% do run_query(sql) %}
 {% endmacro %}
