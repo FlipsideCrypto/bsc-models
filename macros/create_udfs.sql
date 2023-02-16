2ec05b5ce8566234e49ff92a3ff84b31978dd072
xiuy001 <xiuyang@flipsidecrypto.com>
2023-02-16 11:01:45 -0500

    updated

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -6,11 +6,6 @@
 {{ create_udf_hex_to_int(
             schema = "public"
         ) }}
-        {{ create_udtf_get_base_table(
-            schema = "streamline"
-        ) }}
-        {{ create_udf_get_chainhead() }}
-        {{ create_udf_get_blocks() }}
 
         {% endset %}
         {% do run_query(sql) %}
