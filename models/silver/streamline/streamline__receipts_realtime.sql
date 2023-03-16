b624b6b961ffc2c2f59da29f994cfede47ce94ca
xiuy001 <xiuyang@flipsidecrypto.com>
2023-03-16 16:17:02 -0400

    tune the parameters

--- models/silver/streamline/streamline__receipts_realtime.sql
+++ models/silver/streamline/streamline__receipts_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'receipts', 'exploded_key','data', 'method', 'eth_getBlockReceipts', 'producer_batch_size',100, 'producer_limit_size', 1000000, 'worker_batch_size',10, 'producer_batch_chunks_size', 100))",
+        func = "{{this.schema}}.udf_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'receipts', 'exploded_key','data', 'method', 'eth_getBlockReceipts', 'producer_batch_size',1000, 'producer_limit_size', 1000000, 'worker_batch_size',100, 'producer_batch_chunks_size', 1000))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
