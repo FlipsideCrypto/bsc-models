9d61196cea88c5197b7060b004e235624b439199
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-04-12 11:12:57 -0400

    improve view logic (#42)

--- models/gold/core__ez_bnb_transfers.sql
+++ models/gold/core__ez_bnb_transfers.sql
@@ -2,38 +2,6 @@
     materialized = 'view'
 ) }}
 
-WITH bnb_base AS (
-
-    SELECT
-        tx_hash,
-        block_number,
-        block_timestamp,
-        from_address,
-        to_address,
-        bnb_value,
-        identifier,
-        _call_id,
-        ingested_at,
-        input
-    FROM
-        {{ ref('silver__traces') }}
-    WHERE
-        bnb_value > 0
-        AND tx_status = 'SUCCESS'
-        AND gas_used IS NOT NULL
-),
-token_prices AS (
-    SELECT
-        HOUR,
-        price AS bnb_price
-    FROM
-        {{ source(
-            'ethereum',
-            'fact_hourly_token_prices'
-        ) }}
-    WHERE
-        token_address = LOWER('0x418D75f65a02b3D53B2418FB8E1fe493759c7605')
-)
 SELECT
     A.tx_hash AS tx_hash,
     A.block_number AS block_number,
@@ -46,16 +14,25 @@ SELECT
     A.to_address AS bnb_to_address,
     A.bnb_value AS amount,
     ROUND(
-        A.bnb_value * bnb_price,
+        A.bnb_value * price,
         2
     ) AS amount_usd
 FROM
-    bnb_base A
-    LEFT JOIN token_prices
+    {{ ref('silver__traces') }} A
+    LEFT JOIN {{ source(
+        'ethereum',
+        'fact_hourly_token_prices'
+    ) }}
     ON DATE_TRUNC(
         'hour',
-        block_timestamp
+        A.block_timestamp
     ) = HOUR
+    AND token_address = LOWER('0x418D75f65a02b3D53B2418FB8E1fe493759c7605')
     JOIN {{ ref('silver__transactions') }}
     tx
-    ON A.tx_hash = tx.tx_hash
+    ON A.block_timestamp :: DATE = tx.block_timestamp :: DATE
+    AND A.tx_hash = tx.tx_hash
+WHERE
+    A.bnb_value > 0
+    AND A.tx_status = 'SUCCESS'
+    AND A.gas_used IS NOT NULL
