dcc540bdcd0ab9b322d351a16740d873c35795d7
Austin <austin@flipsidecrypto.com>
2023-04-18 10:31:59 -0400

    coalesce

--- models/silver/streamline/decoder/streamline__complete_decode_logs.sql
+++ models/silver/streamline/decoder/streamline__complete_decode_logs.sql
@@ -19,7 +19,7 @@ WITH meta AS (
                 table_name => '{{ source( "bronze_streamline", "decoded_logs") }}',
                 start_time => (
                     SELECT
-                        MAX(_INSERTED_TIMESTAMP)
+                        coalesce(MAX(_INSERTED_TIMESTAMP), '1970-01-01') as _INSERTED_TIMESTAMP
                     FROM
                         {{ this }}
                 )
