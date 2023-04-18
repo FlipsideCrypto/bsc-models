e915c13a0696d4b2e620abb5927c14e15743b5d2
xiuy001 <xiuyang@flipsidecrypto.com>
2023-04-18 10:54:11 -0400

    updated

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,11 +1,17 @@
 {% macro create_aws_bsc_api() %}
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_bsc_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/snowflake-api-bsc' api_allowed_prefixes = (
-            'https://2ltt1xstoc.execute-api.us-east-1.amazonaws.com/prod/',
-            'https://qqy8pvhork.execute-api.us-east-1.amazonaws.com/dev/'
+        CREATE api integration IF NOT EXISTS aws_bsc_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bsc-api-prod-rolesnowflakeudfsAF733095-1GUY4JJNG9FQL' api_allowed_prefixes = (
+            'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
-{% endset %}
+        {% endset %}
+        {% do run_query(sql) %}
+    {% else %}
+        {% set sql %}
+        CREATE api integration IF NOT EXISTS aws_bsc_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/bsc-api-dev-rolesnowflakeudfsAF733095-1S8A1YYLZ09VX' api_allowed_prefixes = (
+            'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/'
+        ) enabled = TRUE;
+        {% endset %}
         {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,8 +1,8 @@
 {% macro create_udf_get_chainhead() %}
     CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_bsc_api AS {% if target.name == "prod" %}
-        'https://2ltt1xstoc.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
-        'https://qqy8pvhork.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {%- endif %};
 {% endmacro %}
 
@@ -10,8 +10,57 @@
     CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc(
         json OBJECT
     ) returns ARRAY api_integration = aws_bsc_api AS {% if target.name == "prod" %}
-        'https://2ltt1xstoc.execute-api.us-east-1.amazonaws.com/prod/bulk_get_json_rpc'
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/bulk_get_json_rpc'
     {% else %}
-        'https://qqy8pvhork.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/bulk_get_json_rpc'
+    {%- endif %};
+{% endmacro %}
+
+
+{% macro create_udf_api() %}
+    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_api(
+        method VARCHAR,
+        url VARCHAR,
+        headers OBJECT,
+        DATA OBJECT
+    ) returns variant api_integration = aws_bsc_api AS {% if target.name == "prod" %}
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/udf_api'
+    {% else %}
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/udf_api'
+    {%- endif %};
+{% endmacro %}
+
+{% macro create_udf_decode_array_string() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode(
+        abi ARRAY,
+        DATA STRING
+    ) returns ARRAY api_integration = aws_bsc_api AS {% if target.name == "prod" %}
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/decode_function'
+    {% else %}
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/decode_function'
+    {%- endif %};
+{% endmacro %}
+
+{% macro create_udf_decode_array_object() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode(
+        abi ARRAY,
+        DATA OBJECT
+    ) returns ARRAY api_integration = aws_bsc_api AS {% if target.name == "prod" %}
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/decode_log'
+    {% else %}
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/decode_log'
+    {%- endif %};
+{% endmacro %}
+
+{% macro create_udf_bulk_decode_logs() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_decode_logs(
+        json OBJECT
+    ) returns ARRAY api_integration = aws_bsc_api AS {% if target.name == "prod" %}
+        'https://u0bch3zf8l.execute-api.us-east-1.amazonaws.com/prod/bulk_decode_logs'
+    {% else %}
+        'https://q22a7542fk.execute-api.us-east-1.amazonaws.com/dev/bulk_decode_logs'
     {%- endif %};
 {% endmacro %}
