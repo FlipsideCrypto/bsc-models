331f241b7fd3ee82a4c6d963f5f82bdaef0b4acf
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2023-05-10 14:10:39 -0400

    AN-3176 add manual seed for abis (#64)
    * add manual seed for abis
    
    * spelling

--- models/silver/API_udf/silver__relevant_abi_contracts.sql
+++ models/silver/API_udf/silver__relevant_abi_contracts.sql
@@ -16,6 +16,12 @@ proxies AS (
     FROM
         {{ ref('silver__proxies') }}
         JOIN base USING (contract_address)
+),
+manual_addresses AS (
+    SELECT
+        contract_address
+    FROM
+        {{ ref('bronze__manual_abi_addresses') }}
 )
 SELECT
     contract_address
@@ -26,3 +32,8 @@ SELECT
     proxy_address AS contract_address
 FROM
     proxies
+UNION
+SELECT
+    contract_address
+FROM
+    manual_addresses
