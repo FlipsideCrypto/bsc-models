ae5856d38808530af57cc291425990a49ffee430
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-05-18 12:00:50 -0400

    migrate decoding (#71)

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -15,6 +15,12 @@
         {{ create_udf_decode_array_string() }}
         {{ create_udf_decode_array_object() }}
         {{ create_udf_bulk_decode_logs() }}
+        {{ create_udf_keccak(
+            schema = 'silver'
+        ) }}
+        {{ create_udf_simple_event_names(
+            schema = 'silver'
+        ) }}
 
         {% endset %}
         {% do run_query(sql) %}
