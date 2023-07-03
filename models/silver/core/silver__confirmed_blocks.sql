64c07c5005d10d5c60bce1d470806e508ea472a5
Austin <austin@flipsidecrypto.com>
2023-07-03 16:58:47 -0400

    use complete  source

--- models/silver/core/silver__confirmed_blocks.sql
+++ models/silver/core/silver__confirmed_blocks.sql
@@ -14,8 +14,8 @@ WITH base AS (
         DATA :result :transactions txs,
         _inserted_timestamp
     FROM
+        {# Remove comments after fill
 
-{# Remove comments after fill 
 {% if is_incremental() %}
 {{ ref('bronze__streamline_confirm_blocks') }}
 WHERE
@@ -33,10 +33,11 @@ WHERE
 {% else %}
     {{ ref('bronze__streamline_FR_confirm_blocks') }}
 {% endif %}
+
 #}
 
 {% if is_incremental() %}
-{{ ref('bronze__streamline_confirm_blocks') }}
+{{ ref('bronze__streamline_FR_confirm_blocks') }}
 WHERE
     _partition_by_block_id BETWEEN (
         SELECT
@@ -48,22 +49,23 @@ WHERE
                     ROUND(MAX(block_number), -4) + 500000
                 FROM
                     {{ this }})
-{% else %}
-{{ ref('bronze__streamline_FR_confirm_blocks') }}
-where _partition_by_block_id <= 2500000
-{% endif %}
+                {% else %}
+                    {{ ref('bronze__streamline_FR_confirm_blocks') }}
+                WHERE
+                    _partition_by_block_id <= 2500000
+                {% endif %}
 
-qualify(ROW_NUMBER() over (PARTITION BY block_number
-ORDER BY
-    _inserted_timestamp DESC)) = 1
-)
-SELECT
-    block_number,
-    block_hash,
-    VALUE :: STRING AS tx_hash,
-    _inserted_timestamp
-FROM
-    base,
-    LATERAL FLATTEN (
-        input => txs
-    )
+                qualify(ROW_NUMBER() over (PARTITION BY block_number
+                ORDER BY
+                    _inserted_timestamp DESC)) = 1
+            )
+        SELECT
+            block_number,
+            block_hash,
+            VALUE :: STRING AS tx_hash,
+            _inserted_timestamp
+        FROM
+            base,
+            LATERAL FLATTEN (
+                input => txs
+            )
