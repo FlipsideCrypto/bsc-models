538a1a12eb49b93d675bd18fb58091b2d3690ddd
Austin <austin@flipsidecrypto.com>
2023-08-23 09:59:41 -0400

    exclude bad tx

--- models/gold/core/core__fact_traces.sql
+++ models/gold/core/core__fact_traces.sql
@@ -11,14 +11,20 @@ SELECT
     from_address,
     to_address,
     bnb_value,
-    IFNULL(
-        bnb_value_precise_raw,
-        '0'
-    ) AS bnb_value_precise_raw,
-    IFNULL(
-        bnb_value_precise,
-        '0'
-    ) AS bnb_value_precise,
+    CASE
+        WHEN tx_hash <> '0xb0db99da6d39abd0bb6c169a4ec472438340a011fd646bdce0c7c99232502301' THEN IFNULL(
+            bnb_value_precise_raw,
+            '0'
+        )
+        ELSE NULL
+    END AS bnb_value_precise_raw,
+    CASE
+        WHEN tx_hash <> '0xb0db99da6d39abd0bb6c169a4ec472438340a011fd646bdce0c7c99232502301' THEN IFNULL(
+            bnb_value_precise,
+            '0'
+        )
+        ELSE NULL
+    END AS bnb_value_precise,
     gas,
     gas_used,
     input,
@@ -59,19 +65,21 @@ FROM
                 ),
                 '0x'
             ) AS hex,
-            to_varchar(TO_NUMBER(hex, REPEAT('X', LENGTH(hex)))) AS bnb_value_precise_raw,
-            IFF(LENGTH(bnb_value_precise_raw) > 18, LEFT(bnb_value_precise_raw, LENGTH(bnb_value_precise_raw) - 18) || '.' || RIGHT(bnb_value_precise_raw, 18), '0.' || LPAD(bnb_value_precise_raw, 18, '0')) AS rough_conversion,
-            IFF(
-                POSITION(
-                    '.000000000000000000' IN rough_conversion
-                ) > 0,
-                LEFT(rough_conversion, LENGTH(rough_conversion) - 19),
-                REGEXP_REPLACE(
-                    rough_conversion,
-                    '0*$',
-                    ''
-                )
-            ) AS bnb_value_precise
-        FROM
-            {{ ref('silver__traces') }}
-    )
+            CASE
+                WHEN tx_hash <> '0xb0db99da6d39abd0bb6c169a4ec472438340a011fd646bdce0c7c99232502301' THEN to_varchar(TO_NUMBER(hex, REPEAT('X', LENGTH(hex))))
+                ELSE null end AS bnb_value_precise_raw,
+                IFF(LENGTH(bnb_value_precise_raw) > 18, LEFT(bnb_value_precise_raw, LENGTH(bnb_value_precise_raw) - 18) || '.' || RIGHT(bnb_value_precise_raw, 18), '0.' || LPAD(bnb_value_precise_raw, 18, '0')) AS rough_conversion,
+                IFF(
+                    POSITION(
+                        '.000000000000000000' IN rough_conversion
+                    ) > 0,
+                    LEFT(rough_conversion, LENGTH(rough_conversion) - 19),
+                    REGEXP_REPLACE(
+                        rough_conversion,
+                        '0*$',
+                        ''
+                    )
+                ) AS bnb_value_precise
+                FROM
+                    {{ ref('silver__traces') }}
+            )
