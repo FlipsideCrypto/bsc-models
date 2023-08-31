150e0ec616c925370ee6d98ebe41fab387bd17b4
mattromano <mattromano88@gmail.com>
2023-08-31 10:02:52 -0700

    add decimals

--- models/silver/prices/silver__hourly_prices_priority.sql
+++ models/silver/prices/silver__hourly_prices_priority.sql
@@ -11,13 +11,15 @@ SELECT
     p.is_imputed,
     p._inserted_timestamp,
     m.symbol AS symbol,
-    NULL AS decimals
+    c.decimals AS decimals
 FROM
     {{ ref('bronze__hourly_prices_priority') }}
     p
     LEFT JOIN {{ ref('silver__asset_metadata_priority') }}
     m
     ON p.token_address = m.token_address
+    LEFT JOIN {{ ref('silver__contracts') }} C
+    ON p.token_address = C.address
 WHERE
     1 = 1
 {% if is_incremental() %}
