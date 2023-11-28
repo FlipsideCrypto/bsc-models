fc3d6e19a782d3fd126847a7fbb38d22659447a9
Austin <austin@flipsidecrypto.com>
2023-11-28 12:51:20 -0500

    stash

--- models/gold/core/core__fact_transactions.sql
+++ models/gold/core/core__fact_transactions.sql
@@ -14,67 +14,34 @@ SELECT
     origin_function_signature,
     from_address,
     to_address,
-    bnb_value,
-    bnb_value_precise_raw,
-    bnb_value_precise,
+    VALUE,
+    value_precise_raw,
+    value_precise,
     tx_fee,
     tx_fee_precise,
     gas_price,
-    gas_limit,
+    gas AS gas_limit,
     gas_used,
     cumulative_gas_used,
     input_data,
-    status,
+    tx_status AS status,
     effective_gas_price,
     max_fee_per_gas,
     max_priority_fee_per_gas,
     r,
     s,
     v,
-    tx_type
+    tx_type,
+    COALESCE (
+        transactions_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['tx_hash']
+        ) }}
+    ) AS fact_transactions_id,
+    GREATEST(COALESCE(A.inserted_timestamp, '2000-01-01'), COALESCE(b.inserted_timestamp, '2000-01-01')) AS inserted_timestamp,
+    GREATEST(COALESCE(A.modified_timestamp, '2000-01-01'), COALESCE(b.modified_timestamp, '2000-01-01')) AS modified_timestamp,
+    VALUE AS bnb_value,
+    value_precise_raw AS bnb_value_precise_raw,
+    value_precise AS bnb_value_precise
 FROM
-    (
-        SELECT
-            block_number,
-            block_timestamp,
-            block_hash,
-            tx_hash,
-            nonce,
-            POSITION,
-            origin_function_signature,
-            from_address,
-            to_address,
-            VALUE AS bnb_value,
-            tx_fee,
-            tx_fee_precise,
-            gas_price,
-            gas AS gas_limit,
-            gas_used,
-            cumulative_gas_used,
-            input_data,
-            tx_status AS status,
-            effective_gas_price,
-            max_fee_per_gas,
-            max_priority_fee_per_gas,
-            r,
-            s,
-            v,
-            tx_type,
-            to_varchar(
-                TO_NUMBER(REPLACE(DATA :value :: STRING, '0x'), REPEAT('X', LENGTH(REPLACE(DATA :value :: STRING, '0x'))))
-            ) AS bnb_value_precise_raw,
-            IFF(LENGTH(bnb_value_precise_raw) > 18, LEFT(bnb_value_precise_raw, LENGTH(bnb_value_precise_raw) - 18) || '.' || RIGHT(bnb_value_precise_raw, 18), '0.' || LPAD(bnb_value_precise_raw, 18, '0')) AS rough_conversion,
-            IFF(
-                POSITION(
-                    '.000000000000000000' IN rough_conversion
-                ) > 0,
-                LEFT(rough_conversion, LENGTH(rough_conversion) - 19),
-                REGEXP_REPLACE(
-                    rough_conversion,
-                    '0*$',
-                    ''
-                )
-            ) AS bnb_value_precise
-        FROM
-            {{ ref('silver__transactions') }}
-    )
+    {{ ref('silver__transactions') }}
