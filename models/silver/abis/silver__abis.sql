373808336b611417e8d2359352b15b4cda8872b8
drethereum <trevor.wenokur@gmail.com>
2023-12-04 16:34:53 -0700

    bscscan

--- models/silver/abis/silver__abis.sql
+++ models/silver/abis/silver__abis.sql
@@ -33,7 +33,7 @@ verified_abis AS (
     FROM
         {{ ref('silver__verified_abis') }}
     WHERE
-        abi_source = 'opscan'
+        abi_source = 'bscscan'
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -44,7 +44,7 @@ AND _inserted_timestamp >= (
     FROM
         {{ this }}
     WHERE
-        abi_source = 'opscan'
+        abi_source = 'bscscan'
 )
 {% endif %}
 ),
