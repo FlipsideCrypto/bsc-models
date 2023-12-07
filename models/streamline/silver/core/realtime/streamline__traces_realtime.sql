ff5e5a1f2a5fab5e17c355676de54e026737c990
xiuy001 <xiuyang@flipsidecrypto.com>
2023-12-07 11:25:14 -0500

    updated

--- models/streamline/silver/core/realtime/streamline__traces_realtime.sql
+++ models/streamline/silver/core/realtime/streamline__traces_realtime.sql
@@ -1,117 +1,21 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_json_rpc(object_construct('batch_call','non_batch_call', 'sql_source', '{{this.identifier}}', 'external_table', 'traces', 'recursive', 'full', 'exploded_key','[\"result\", \"calls\"]', 'method', 'debug_traceBlockByNumber', 'producer_batch_size',100, 'producer_limit_size', 1000, 'worker_batch_size',10))",
+        func = "{{this.schema}}.udf_json_rpc(object_construct('batch_call','non_batch_call', 'sql_source', '{{this.identifier}}', 'external_table', 'traces', 'recursive', 'true', 'exploded_key','[\"result\", \"calls\"]', 'method', 'debug_traceBlockByNumber', 'producer_batch_size',100, 'producer_limit_size', 1000, 'worker_batch_size',10))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_core_realtime']
 ) }}
 
-WITH last_3_days AS (
-
-    SELECT
-        block_number
-    FROM
-        {{ ref("_block_lookback") }}
-),
-tbl AS (
-    SELECT
-        block_number,
-        block_number_hex,
-        'debug_traceBlockByNumber' AS method,
-        CONCAT(
-            block_number_hex,
-            '_-_',
-            '{"tracer": "callTracer"}'
-        ) AS params
-    FROM
-        {{ ref("streamline__blocks") }}
-    WHERE
-        block_number IS NOT NULL
-        AND (
-            block_number >= (
-                SELECT
-                    block_number
-                FROM
-                    last_3_days
-            )
-        )
-    EXCEPT
-    SELECT
-        block_number,
+SELECT
+    33560928 AS block_number,
+    'debug_traceBlockByNumber' AS method,
+    CONCAT(
         REPLACE(
-            concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
+            concat_ws('', '0x', to_char(33560928, 'XXXXXXXX')),
             ' ',
             ''
-        ) AS block_number_hex,
-        'debug_traceBlockByNumber' AS method,
-        CONCAT(
-            REPLACE(
-                concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
-                ' ',
-                ''
-            ),
-            '_-_',
-            '{"tracer": "callTracer"}'
-        ) AS params
-    FROM
-        {{ ref("streamline__complete_traces") }}
-    WHERE
-        block_number IS NOT NULL
-        AND _inserted_timestamp >= DATEADD(
-            'day',
-            -4,
-            SYSDATE()
-        )
-        AND (
-            block_number >= (
-                SELECT
-                    block_number
-                FROM
-                    last_3_days
-            )
-        )
-),
-retry_blocks AS (
-    SELECT
-        block_number,
-        'debug_traceBlockByNumber' AS method,
-        CONCAT(
-            REPLACE(
-                concat_ws('', '0x', to_char(block_number, 'XXXXXXXX')),
-                ' ',
-                ''
-            ),
-            '_-_',
-            '{"tracer": "callTracer"}'
-        ) AS params
-    FROM
-        (
-            SELECT
-                block_number
-            FROM
-                {{ ref("_missing_traces") }}
-            UNION
-            SELECT
-                block_number
-            FROM
-                {{ ref("_unconfirmed_blocks") }}
-        )
-)
-SELECT
-    block_number,
-    method,
-    params
-FROM
-    tbl
-UNION
-SELECT
-    block_number,
-    method,
-    params
-FROM
-    retry_blocks
-ORDER BY
-    block_number ASC
-LIMIT
-    1200
+        ),
+        '_-_',
+        '{"tracer": "callTracer"}'
+    ) AS params
