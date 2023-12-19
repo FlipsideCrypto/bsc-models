91a8d1f6566942482a64e786008e680b879da078
Matt Romano <42412983+mattromano@users.noreply.github.com>
2023-12-19 09:09:21 -0800

    AN-3833/complete-lending-curation (#193)
    * venus tokens
    
    * add venus and radiant
    
    * quick fixes
    
    * add dforce
    
    * add liqee and dforce fixes
    
    * quick fixes
    
    * add kinza
    
    * move venus and liqee back to itoken, silver complete lending working
    
    * add gold layer
    
    * test clean up
    
    * test fixes
    
    * liqee asset fix + documentation updates
    
    * add new column desc
    
    * add premium amount to flashloans
    
    * unique topic fixes
    
    * dforce fix
    
    * flashloan fix
    
    * remove flashloan and liquidation recent tests
    
    * missed 12 hour inc
    
    * pr comment fixes
    
    * asset details model optimizations
    
    * move silver dex under silver defi folder

---
+++ models/silver/defi/lending/complete_lending/silver__complete_lending_liquidations.sql
@@ -0,0 +1,279 @@
+{{ config(
+  materialized = 'incremental',
+  incremental_strategy = 'delete+insert',
+  unique_key = ['block_number','platform'],
+  cluster_by = ['block_timestamp::DATE'],
+  tags = ['non_realtime','reorg','curated']
+) }}
+
+WITH liquidation_union AS (
+
+  SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    event_index,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    contract_address,
+    liquidator,
+    borrower,
+    amount_unadj,
+    amount,
+
+    itoken AS protocol_collateral_asset,
+    liquidation_contract_address AS collateral_asset,
+    liquidation_contract_symbol AS collateral_asset_symbol,
+    collateral_token AS debt_asset,
+    collateral_symbol AS debt_asset_symbol,
+    platform,
+    'bsc' AS blockchain,
+    _LOG_ID,
+    _INSERTED_TIMESTAMP
+  FROM
+    {{ ref('silver__venus_liquidations') }}
+    l
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+UNION ALL
+SELECT
+  tx_hash,
+  block_number,
+  block_timestamp,
+  event_index,
+  origin_from_address,
+  origin_to_address,
+  origin_function_signature,
+  contract_address,
+  liquidator,
+  borrower,
+  amount_unadj,
+  amount,
+  token AS protocol_collateral_asset,
+  liquidation_contract_address AS collateral_asset,
+  liquidation_contract_symbol AS collateral_asset_symbol,
+  collateral_token AS debt_asset,
+  collateral_symbol AS debt_asset_symbol,
+  platform,
+  'bsc' AS blockchain,
+  _LOG_ID,
+  _INSERTED_TIMESTAMP
+FROM
+  {{ ref('silver__dforce_liquidations') }}
+  l
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+UNION ALL
+SELECT
+  tx_hash,
+  block_number,
+  block_timestamp,
+  event_index,
+  origin_from_address,
+  origin_to_address,
+  origin_function_signature,
+  contract_address,
+  liquidator,
+  borrower,
+  amount_unadj,
+  amount,
+  collateral_kinza_token AS protocol_collateral_asset,
+  collateral_asset,
+  collateral_token_symbol AS collateral_asset_symbol,
+  debt_asset,
+  debt_token_symbol AS debt_asset_symbol,
+  platform,
+  'bsc' AS blockchain,
+  _LOG_ID,
+  _INSERTED_TIMESTAMP
+FROM
+  {{ ref('silver__kinza_liquidations') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+UNION ALL
+SELECT
+  tx_hash,
+  block_number,
+  block_timestamp,
+  event_index,
+  origin_from_address,
+  origin_to_address,
+  origin_function_signature,
+  contract_address,
+  liquidator,
+  borrower,
+  amount_unadj,
+  amount,
+  collateral_radiant_token AS protocol_collateral_asset,
+  collateral_asset,
+  collateral_token_symbol AS collateral_asset_symbol,
+  debt_asset,
+  debt_token_symbol AS debt_asset_symbol,
+  platform,
+  'bsc' AS blockchain,
+  _LOG_ID,
+  _INSERTED_TIMESTAMP
+FROM
+  {{ ref('silver__radiant_liquidations') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+UNION ALL
+SELECT
+  tx_hash,
+  block_number,
+  block_timestamp,
+  event_index,
+  origin_from_address,
+  origin_to_address,
+  origin_function_signature,
+  contract_address,
+  liquidator,
+  borrower,
+  amount_unadj,
+  amount,
+  itoken AS protocol_collateral_asset,
+  liquidation_contract_address AS collateral_asset,
+  liquidation_contract_symbol AS collateral_asset_symbol,
+  collateral_token AS debt_asset,
+  collateral_symbol AS debt_asset_symbol,
+  platform,
+  'bsc' AS blockchain,
+  _LOG_ID,
+  _INSERTED_TIMESTAMP
+FROM
+  {{ ref('silver__venus_liquidations') }}
+  l
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+UNION ALL
+SELECT
+  tx_hash,
+  block_number,
+  block_timestamp,
+  event_index,
+  origin_from_address,
+  origin_to_address,
+  origin_function_signature,
+  contract_address,
+  liquidator,
+  borrower,
+  amount_unadj,
+  amount,
+  itoken AS protocol_collateral_asset,
+  liquidation_contract_address AS collateral_asset,
+  liquidation_contract_symbol AS collateral_asset_symbol,
+  collateral_token AS debt_asset,
+  collateral_symbol AS debt_asset_symbol,
+  platform,
+  'bsc' AS blockchain,
+  _LOG_ID,
+  _INSERTED_TIMESTAMP
+FROM
+  {{ ref('silver__liqee_liquidations') }}
+  
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '36 hours'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+),
+FINAL AS (
+  SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    event_index,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    A.contract_address,
+    CASE
+      WHEN platform in ('dForce','Liqee','Venus')  THEN 'LiquidateBorrow'
+      ELSE 'LiquidationCall'
+    END AS event_name,
+    liquidator,
+    borrower,
+    protocol_collateral_asset AS protocol_market,
+    collateral_asset AS collateral_token,
+    collateral_asset_symbol AS collateral_token_symbol,
+    amount_unadj,
+    amount,
+    ROUND(
+      amount * p.price,
+      2
+    ) AS amount_usd,
+    debt_asset AS debt_token,
+    debt_asset_symbol AS debt_token_symbol,
+    platform,
+    A.blockchain,
+    A._LOG_ID,
+    A._INSERTED_TIMESTAMP
+  FROM
+    liquidation_union A
+    LEFT JOIN {{ ref('price__ez_hourly_token_prices') }} P
+    ON collateral_asset = p.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p.hour
+    LEFT JOIN {{ ref('silver__contracts') }} C
+    ON collateral_asset = C.contract_address
+)
+SELECT
+  *,
+  {{ dbt_utils.generate_surrogate_key(
+    ['tx_hash','event_index']
+  ) }} AS complete_lending_liquidations_id,
+  SYSDATE() AS inserted_timestamp,
+  SYSDATE() AS modified_timestamp,
+  '{{ invocation_id }}' AS _invocation_id
+FROM
+  FINAL qualify(ROW_NUMBER() over(PARTITION BY _log_id
+ORDER BY
+  _inserted_timestamp DESC)) = 1
