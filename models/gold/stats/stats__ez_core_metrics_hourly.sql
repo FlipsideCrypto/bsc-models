6cf53ed224bbb1703f17cc7abe7212b428015ddb
drethereum <trevor.wenokur@gmail.com>
2024-01-29 16:52:48 -0700

    native

--- models/gold/stats/stats__ez_core_metrics_hourly.sql
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -16,7 +16,7 @@ SELECT
     transaction_count_failed,
     unique_from_count,
     unique_to_count,
-    total_fees AS total_fees_eth,
+    total_fees AS total_fees_native,
     total_fees * p.price AS total_fees_usd,
     COALESCE (
         core_metrics_hourly_id,
@@ -38,4 +38,4 @@ FROM
     LEFT JOIN {{ ref('price__ez_hourly_token_prices') }}
     p
     ON s.block_timestamp_hour = p.hour
-    AND p.token_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --WETH
+    AND p.token_address = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c' --WBNB
