2864d3e98f1d1c97c0a42f825d617effb1f4311d
mattromano <mattromano88@gmail.com>
2024-02-15 17:35:54 -0800

    remove tag

--- models/silver/defi/lending/radiant/silver__radiant_liquidations.sql
+++ models/silver/defi/lending/radiant/silver__radiant_liquidations.sql
@@ -2,8 +2,7 @@
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
-    cluster_by = ['block_timestamp::DATE'],
-    tags = ['reorg','curated']
+    cluster_by = ['block_timestamp::DATE']
 ) }}
 
 WITH liquidation AS(
