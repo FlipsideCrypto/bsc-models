98271580f2a4766dab1fbdcc1a2b11db08f634ac
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-09-11 23:59:10 -0400

    regular logic (#286)
    * regular logic
    
    * add predicate

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -1,14 +1,15 @@
 {{ config (
     materialized = "incremental",
     incremental_strategy = 'delete+insert',
-    unique_key = ['block_number'],
+    unique_key = "block_number",
     cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    tags = ['traces_reload']
+    tags = ['core','non_realtime'],
+    incremental_predicates = [standard_predicate()],
+    full_refresh = false
 ) }}
 {{ fsc_evm.gold_traces_v1(
     full_reload_start_block = 5000000,
     full_reload_blocks = 1000000,
-    full_reload_mode = true,
     uses_overflow_steps = true
 ) }}
