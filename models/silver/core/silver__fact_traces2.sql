91a9c1ba36e42b460215b5f81197dd361041c3ca
Austin <austin@flipsidecrypto.com>
2024-09-12 11:04:26 -0400

    predicate

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -2,10 +2,10 @@
     materialized = "incremental",
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
+    incremental_predicates = [fsc_evm.standard_predicate()],
     cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
     tags = ['core','non_realtime'],
-    incremental_predicates = [standard_predicate()],
     full_refresh = false
 ) }}
 {{ fsc_evm.gold_traces_v1(
