9dd55181e70972e3a0a14917a985e23c75e5e333
drethereum <trevor.wenokur@gmail.com>
2025-05-07 14:50:41 -0600

    sl

--- models/streamline/bronze/core/bronze__blocks_fr.sql
+++ models/streamline/bronze/core/bronze__blocks_fr.sql
@@ -1,6 +1,9 @@
+{# Log configuration details #}
+{{ fsc_evm.log_model_details() }}
+
 {{ config (
     materialized = 'view',
-    tags = ['bronze_core']
+    tags = ['bronze','core','phase_1']
 ) }}
 
 SELECT
@@ -13,29 +16,14 @@ SELECT
     _inserted_timestamp
 FROM
     {{ ref('bronze__blocks_fr_v2') }}
-{% if var('GLOBAL_USES_STREAMLINE_V1', false) %}
 UNION ALL
 SELECT
     _partition_by_block_id AS partition_key,
     block_number,
     VALUE,
-    DATA :result AS DATA, --add to fsc-evm with if logic and new var, specific to BSC only
+    DATA :result AS DATA,
     metadata,
     file_name,
     _inserted_timestamp
 FROM
    {{ ref('bronze__blocks_fr_v1') }}
-{% endif %}
-{% if var('GLOBAL_USES_BLOCKS_TRANSACTIONS_PATH', false) %}
-UNION ALL
-SELECT
-    partition_key,
-    block_number,
-    VALUE,
-    DATA,
-    metadata,
-    file_name,
-    _inserted_timestamp
-FROM
-    {{ ref('bronze__blocks_fr_v2_1') }}
-{% endif %}
