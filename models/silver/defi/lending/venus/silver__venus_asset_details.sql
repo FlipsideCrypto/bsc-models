a73352997491f564ef62d7fe25f4f694ebb34049
drethereum <trevor.wenokur@gmail.com>
2025-05-13 14:39:24 -0600

    curated tweaks

--- models/silver/defi/lending/venus/silver__venus_asset_details.sql
+++ models/silver/defi/lending/venus/silver__venus_asset_details.sql
@@ -63,7 +63,7 @@ traces_pull AS (
         t.from_address AS token_address,
         t.to_address AS underlying_asset,
         CASE
-            WHEN identifier = 'STATICCALL_0_2' THEN 1
+            WHEN CONCAT(TYPE,'_',TRACE_ADDRESS) = 'STATICCALL_0_2' THEN 1
             ELSE NULL
         END AS asset_identifier
     FROM
